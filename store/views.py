c6eb7b2a229cdae9de027690601ae73a3ef7ff87
Kravets83 <97585334+Kravets83@users.noreply.github.com>
2022-07-12 17:34:21 +0300

    add context processor

--- store/views.py
+++ store/views.py
@@ -2,12 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 
-def categories(request):
-    return {
-        'categories': Category.objects.all(),
-    }
-
-
 def products(request):
     return {
         'products': Product.objects.all(),
@@ -28,8 +22,3 @@ def category_list(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=category)
     return render(request, 'store/products/category.html', {'category': category, 'products': products})
-
-
-
-
-
